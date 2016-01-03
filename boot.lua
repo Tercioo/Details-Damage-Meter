518cb8a8acdaea7286b370e255396bd2f49c661e
Tercioo <Tercioo@localhost>
2016-01-03 20:50:55 -0200

    - repository fixed, I think.

--- boot.lua
+++ boot.lua
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --> global name declaration
- 
+
 		_ = nil
 		_detalhes = LibStub("AceAddon-3.0"):NewAddon("_detalhes", "AceTimer-3.0", "AceComm-3.0", "AceSerializer-3.0", "NickTag-1.0")
 		_detalhes.build_counter = 2186 --it's 2186 for release
-		_detalhes.userversion = "v4.1.4"
+		_detalhes.userversion = "v4.1.4a"
 		_detalhes.realversion = 77 --core version
 		_detalhes.version = _detalhes.userversion .. " (core " .. _detalhes.realversion .. ")"
 		Details = _detalhes
