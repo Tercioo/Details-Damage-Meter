31241bd00da5341841779eedebf6a27ba2b44d2a
Tercio <Tercio@localhost>
2017-05-29 20:39:31 -0300

    - Fixed an error while killing low level mobs with warrior class.

--- classes/classe_others.lua
+++ classes/classe_others.lua
@@ -1301,18 +1301,20 @@ function _detalhes:CatchRaidDebuffUptime (in_or_out) -- "DEBUFF_UPTIME_IN"
 		
 	else
 		local his_target = _UnitGUID ("playertarget")
-		
-		if (his_target and UnitReaction ("playertarget", "player") <= 4) then
-			for debuffIndex = 1, 40 do
-				local name, _, _, _, _, _, _, unitCaster, _, _, spellid  = UnitDebuff ("playertarget", debuffIndex)
-				if (name and unitCaster) then
-					local playerName, realmName = _UnitName (unitCaster)
-					local playerGUID = _UnitGUID (unitCaster)
-					if (playerGUID) then
-						if (realmName and realmName ~= "") then
-							playerName = playerName .. "-" .. realmName
+		if (his_target) then
+			local reaction = UnitReaction ("playertarget", "player")
+			if (reaction and reaction <= 4) then
+				for debuffIndex = 1, 40 do
+					local name, _, _, _, _, _, _, unitCaster, _, _, spellid  = UnitDebuff ("playertarget", debuffIndex)
+					if (name and unitCaster) then
+						local playerName, realmName = _UnitName (unitCaster)
+						local playerGUID = _UnitGUID (unitCaster)
+						if (playerGUID) then
+							if (realmName and realmName ~= "") then
+								playerName = playerName .. "-" .. realmName
+							end
+							_detalhes.parser:add_debuff_uptime (nil, GetTime(), playerGUID, playerName, 0x00000417, his_target, _UnitName ("playertarget"), 0x842, nil, spellid, name, in_or_out)
 						end
-						_detalhes.parser:add_debuff_uptime (nil, GetTime(), playerGUID, playerName, 0x00000417, his_target, _UnitName ("playertarget"), 0x842, nil, spellid, name, in_or_out)
 					end
 				end
 			end
