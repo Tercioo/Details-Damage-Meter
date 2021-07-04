ec4d1f06cc0f63c72d44f65ec52de90e17013fec
Tercio Jose <terciob19@hotmail.com>
2021-07-04 18:53:06 -0300

    version up

--- boot.lua
+++ boot.lua
@@ -6,8 +6,8 @@
 		
 		local version, build, date, tocversion = GetBuildInfo()
 
-		_detalhes.build_counter = 8697
-		_detalhes.alpha_build_counter = 8697 --if this is higher than the regular counter, use it instead
+		_detalhes.build_counter = 8698
+		_detalhes.alpha_build_counter = 8698 --if this is higher than the regular counter, use it instead
 		_detalhes.bcc_counter = 25
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
