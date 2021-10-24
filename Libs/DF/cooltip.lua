54916604484d50fb9ab9b7bdeea48898b0b6dbb7
Tercio Jose <terciob19@hotmail.com>
2021-10-24 14:32:42 -0300

    Fix for an error on classic era SoM

--- Libs/DF/cooltip.lua
+++ Libs/DF/cooltip.lua
@@ -274,7 +274,7 @@ function DF:CreateCoolTip()
 		--> main frame
 		local frame1
 		if (not GameCooltipFrame1) then
-			frame1 = CreateFrame ("Frame", "GameCooltipFrame1", UIParent,"TooltipBackdropTemplate")
+			frame1 = CreateFrame ("Frame", "GameCooltipFrame1", UIParent, "TooltipBackdropTemplate, BackdropTemplate")
 			
 			tinsert (UISpecialFrames, "GameCooltipFrame1")
 			DF:CreateFlashAnimation (frame1)
@@ -302,7 +302,7 @@ function DF:CreateCoolTip()
 		--> secondary frame
 		local frame2
 		if (not GameCooltipFrame2) then
-			frame2 = CreateFrame ("Frame", "GameCooltipFrame2", UIParent,"TooltipBackdropTemplate")
+			frame2 = CreateFrame ("Frame", "GameCooltipFrame2", UIParent,"TooltipBackdropTemplate, BackdropTemplate")
 			
 			tinsert (UISpecialFrames, "GameCooltipFrame2")
 			DF:CreateFlashAnimation (frame2)
