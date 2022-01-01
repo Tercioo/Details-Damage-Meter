e6f75ac9bfe663bdc6982c05283c6a17647d73c2
Tercio Jose <terciob19@hotmail.com>
2022-01-01 16:11:15 -0300

    Officially adding Lib OpenRaid

---
+++ Libs/LibOpenRaid/README.md
@@ -0,0 +1,2 @@
+# Open-Raid-Library
+Library for World of Warcraft game, help to transmit data among players in a group
--- boot.lua
+++ boot.lua
@@ -8,7 +8,7 @@
 
 		_detalhes.build_counter = 9213
 		_detalhes.alpha_build_counter = 9213 --if this is higher than the regular counter, use it instead
-		_detalhes.bcc_counter = 30
+		_detalhes.bcc_counter = 31
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
 		_detalhes.userversion = version .. _detalhes.build_counter
