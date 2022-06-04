c2ad23a64e8f138f337a017ae058c198aa5464c9
Flamanis <Flamanis@users.noreply.github.com>
2022-06-04 06:10:31 -0500

    Hunter Beast Mastery fix for TBC/Classic talents

--- Libs/DF/fw.lua
+++ Libs/DF/fw.lua
@@ -1,6 +1,6 @@
 
 
-local dversion = 316
+local dversion = 317
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary (major, minor)
 
@@ -76,7 +76,7 @@ local roleBySpecTextureName = {
 	DruidFeralCombat = "DAMAGER",
 	DruidRestoration = "HEALER",
 
-	HunterBeastMaster = "DAMAGER",
+	HunterBeastMastery = "DAMAGER",
 	HunterMarksmanship = "DAMAGER",
 	HunterSurvival = "DAMAGER",
 
