78dcde2530a0b96b746ab09c1c225a804fef0745
Tercio Jose <terciob19@hotmail.com>
2022-06-04 13:05:13 -0300

    Framework fixes by Flamanis

--- boot.lua
+++ boot.lua
@@ -6,9 +6,9 @@
 
 		local version, build, date, tocversion = GetBuildInfo()
 
-		_detalhes.build_counter = 9820
-		_detalhes.alpha_build_counter = 9820 --if this is higher than the regular counter, use it instead
-		_detalhes.bcc_counter = 38
+		_detalhes.build_counter = 9821
+		_detalhes.alpha_build_counter = 9821 --if this is higher than the regular counter, use it instead
+		_detalhes.bcc_counter = 39
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
 		_detalhes.userversion = version .. _detalhes.build_counter
