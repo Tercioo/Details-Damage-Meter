fea926f6bd537c976d90ee6d8203e66a9613f247
Daniel Henry <daniel.henry@epiforge.com>
2022-07-20 12:38:37 -0400

    recognize WotLKC as "timewalking" everywhere GetBuildInfo is used

--- Libs/DF/fw.lua
+++ Libs/DF/fw.lua
@@ -41,7 +41,7 @@ local PixelUtil = PixelUtil or DFPixelUtil
 if (not PixelUtil) then
 	--check if is in classic or TBC wow, if it is, build a replacement for PixelUtil
 	local gameVersion = GetBuildInfo()
-	if (gameVersion:match("%d") == "1" or gameVersion:match("%d") == "2") then
+	if (gameVersion:match("%d") == "1" or gameVersion:match("%d") == "2" or gameVersion:match("%d") == "3") then
 		PixelUtil = {
 			SetWidth = function (self, width) self:SetWidth (width) end,
 			SetHeight = function (self, height) self:SetHeight (height) end,
