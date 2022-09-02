dcf62493eb7cda5680c4f2d1a1ca58b4ee72e6e4
Tercio Jose <terciob19@hotmail.com>
2022-09-01 23:33:52 -0300

    15

--- boot.lua
+++ boot.lua
@@ -18,7 +18,7 @@
 		_detalhes.BFACORE = 131 --core version on BFA launch
 		_detalhes.SHADOWLANDSCORE = 143 --core version on Shadowlands launch
 
-		_detalhes.dragonflight_beta_version = 14
+		_detalhes.dragonflight_beta_version = 15
 
 		Details = _detalhes
 
