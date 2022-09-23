7f5ded76b245e4289a6d74ef68de4411df8e5624
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2022-09-23 05:33:48 +0100

    Add more bdk spec spells for WotLK Classic

--- functions/spells.lua
+++ functions/spells.lua
@@ -807,7 +807,9 @@ do
 			-- Blood Death Knight:
 			[53137]			=	250, -- Abomination's Might (rank 1)
 			[53138]			=	250, -- Abomination's Might (rank 2)
+			[50452]			=	250, -- Bloodworms
 			[49016]			=	250, -- Hysteria
+			[55233]			=	250, -- Vampiric Blood
 			[55050]			=	250, -- Heart Strike (rank 1)
 			[55258]			=	250, -- Heart Strike (rank 2)
 			[55259]			=	250, -- Heart Strike (rank 3)
