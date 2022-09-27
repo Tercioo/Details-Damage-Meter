51cc785203aacbb97c5172a0b405dd5d7cb62cc9
Flamanis <Flamanis@users.noreply.github.com>
2022-09-26 20:57:05 -0500

    Update consumables lists for Wrath
    Includes version bump for DF

--- Libs/DF/fw.lua
+++ Libs/DF/fw.lua
@@ -1,6 +1,6 @@
 
 
-local dversion = 367
+local dversion = 368
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary (major, minor)
 
@@ -4931,4 +4931,3 @@ end
 
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
-
