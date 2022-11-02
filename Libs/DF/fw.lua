61e7f2ad52bc05f2e4077fd405bd8235e0893dcb
Tercio Jose <terciob19@hotmail.com>
2022-11-02 13:32:09 -0300

    Release with update to framework

--- Libs/DF/fw.lua
+++ Libs/DF/fw.lua
@@ -1,6 +1,6 @@
 
 
-local dversion = 385
+local dversion = 386
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary(major, minor)
 
--- boot.lua
+++ boot.lua
@@ -6,8 +6,8 @@
 
 		local version, build, date, tocversion = GetBuildInfo()
 
-		_detalhes.build_counter = 10205
-		_detalhes.alpha_build_counter = 10205 --if this is higher than the regular counter, use it instead
+		_detalhes.build_counter = 10258
+		_detalhes.alpha_build_counter = 10258 --if this is higher than the regular counter, use it instead
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
 		_detalhes.userversion = version .. " " .. _detalhes.build_counter
