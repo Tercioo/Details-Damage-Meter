77f8e7cab3e768b903627cb8aa05f6cbcb2dfadb
Tercio Jose <terciob19@hotmail.com>
2023-01-06 19:33:09 -0300

    Fixed an issue with the Streamer! plugin hiding after a logon

--- boot.lua
+++ boot.lua
@@ -6,8 +6,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tocversion = GetBuildInfo()
 
-		_detalhes.build_counter = 10402
-		_detalhes.alpha_build_counter = 10402 --if this is higher than the regular counter, use it instead
+		_detalhes.build_counter = 10403
+		_detalhes.alpha_build_counter = 10403 --if this is higher than the regular counter, use it instead
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
 		_detalhes.userversion = version .. " " .. _detalhes.build_counter
