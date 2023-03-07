20695ea5e2264bcd9581d23384f6bc39c7d13a56
Tercio Jose <terciob19@hotmail.com>
2023-03-07 16:23:02 -0300

    Updates on Framework and Libraries

--- boot.lua
+++ boot.lua
@@ -10,8 +10,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tocversion = GetBuildInfo()
 
-		_detalhes.build_counter = 10661
-		_detalhes.alpha_build_counter = 10661 --if this is higher than the regular counter, use it instead
+		_detalhes.build_counter = 10662
+		_detalhes.alpha_build_counter = 10662 --if this is higher than the regular counter, use it instead
 		_detalhes.dont_open_news = true
 		_detalhes.game_version = version
 		_detalhes.userversion = version .. " " .. _detalhes.build_counter
