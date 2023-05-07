950588fd428b3847a763ebf3e1b86dba3c69245d
Tercio Jose <terciob19@hotmail.com>
2023-05-07 00:21:27 -0300

    Version Bump

--- boot.lua
+++ boot.lua
@@ -15,8 +15,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tocversion = GetBuildInfo()
 
-		Details.build_counter = 10986
-		Details.alpha_build_counter = 10986 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 10989
+		Details.alpha_build_counter = 10989 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
