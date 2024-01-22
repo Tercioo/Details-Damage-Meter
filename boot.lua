d4da0b2f46f088acab50ffef333f9d77b5b98aa6
Tercio Jose <terciob19@hotmail.com>
2024-01-22 15:53:57 -0300

    Tagging release for latest commit which fixed an issue with WotLK.

--- boot.lua
+++ boot.lua
@@ -18,8 +18,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tocversion = GetBuildInfo()
 
-		Details.build_counter = 12236
-		Details.alpha_build_counter = 12236 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 12237
+		Details.alpha_build_counter = 12237 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
