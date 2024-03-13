5f6ad0efd8e20ea563893e2db7758fc112beb1fa
Tercio Jose <terciob19@hotmail.com>
2024-03-13 18:35:49 -0300

    Version Up

--- boot.lua
+++ boot.lua
@@ -18,8 +18,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tocversion = GetBuildInfo()
 
-		Details.build_counter = 12551
-		Details.alpha_build_counter = 12551 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 12552
+		Details.alpha_build_counter = 12552 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
