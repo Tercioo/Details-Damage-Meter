6071ae3e6667fd3209f1d0b88e7b8f9299447f66
Flamanis <Flamanis@users.noreply.github.com>
2024-06-08 16:29:41 -0500

    Update breakdown_spells_spellframes.lua

--- frames/window_breakdown/breakdown_spells_spellframes.lua
+++ frames/window_breakdown/breakdown_spells_spellframes.lua
@@ -336,14 +336,15 @@ local onEnterSpellBar = function(spellBar, motion) --parei aqui: precisa por nom
 			local normalAverage = spellTable.n_total / math.max(normalHitsAmt, 0.0001)
 			blockLine3.leftText:SetText(Loc ["STRING_AVERAGE"] .. ": " .. Details:CommaValue(normalAverage))
 
+            local normalHPS = 0
             if (spellTable.n_total > 0) then
 				local tempo = (elapsedTime * spellTable.n_total) / math.max(spellTable.total, 0.001)
 				local normalAveragePercent = spellBar.average / normalAverage * 100
 				local normalTempoPercent = normalAveragePercent * tempo / 100
-				blockLine3.rightText:SetText(Loc ["STRING_HPS"] .. ": " .. Details:CommaValue(spellTable.n_total / normalTempoPercent))
-			else
-				blockLine3.rightText:SetText(Loc ["STRING_HPS"] .. ": " .. Details:CommaValue(0))
+				normalHPS = spellTable.n_total / normalTempoPercent
 			end
+
+            blockLine3.rightText:SetText(Loc ["STRING_HPS"] .. ": " .. Details:CommaValue(normalHPS))
 		end
 
 		---@type number
@@ -368,10 +369,15 @@ local onEnterSpellBar = function(spellBar, motion) --parei aqui: precisa por nom
 			local critAverage = spellTable.c_total / math.max(criticalHitsAmt, 0.0001)
 			blockLine3.leftText:SetText(Loc ["STRING_AVERAGE"] .. ": " .. Details:CommaValue(critAverage))
 
-			local tempo = (elapsedTime * spellTable.c_total) / math.max(spellTable.total, 0.001)
-			local critAveragePercent = spellBar.average / critAverage * 100
-			local critTempoPercent = critAveragePercent * tempo / 100
-			blockLine3.rightText:SetText(Loc ["STRING_HPS"] .. ": " .. Details:CommaValue(spellTable.c_total / critTempoPercent))
+            local critHPS = 0
+            if (spellTable.c_total > 0) then
+                local tempo = (elapsedTime * spellTable.c_total) / math.max(spellTable.total, 0.001)
+                local critAveragePercent = spellBar.average / critAverage * 100
+                local critTempoPercent = critAveragePercent * tempo / 100
+                critHPS = spellTable.c_total / critTempoPercent
+            end
+
+            blockLine3.rightText:SetText(Loc ["STRING_HPS"] .. ": " .. Details:CommaValue(critHPS))
 		end
 
 		---@type number
