2e1bde48b066e86360cc975560c2112e1dd5702c
Tercio Jose <terciob19@hotmail.com>
2024-07-15 10:51:56 -0300

    tag as release for the new wipe counter and register pets when out of combat

--- boot.lua
+++ boot.lua
@@ -19,8 +19,8 @@
 		local addonName, Details222 = ...
 		local version, build, date, tvs = GetBuildInfo()
 
-		Details.build_counter = 12805
-		Details.alpha_build_counter = 12805 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 12806
+		Details.alpha_build_counter = 12806 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
