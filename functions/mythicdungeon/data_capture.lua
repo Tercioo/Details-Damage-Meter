06c29d5bb0eef2bd4fc2bc07f46384fb1c7eed56
Eivind Kristoffersen <frag6464@gmail.com>
2025-05-24 23:18:11 +0200

    Manual update to match master

--- functions/mythicdungeon/data_capture.lua
+++ functions/mythicdungeon/data_capture.lua
@@ -165,10 +165,9 @@ function mythicDungeonCharts:OnBossDefeated()
 	local currentCombat = Details:GetCurrentCombat()
 	local segmentType = currentCombat:GetCombatType()
 	local bossInfo = currentCombat:GetBossInfo()
-	--local mythicLevel = C_ChallengeMode and C_ChallengeMode.GetActiveKeystoneInfo()
+	local mythicLevel = C_ChallengeMode and C_ChallengeMode.GetActiveKeystoneInfo and C_ChallengeMode.GetActiveKeystoneInfo()
 
-	--if (mythicLevel and mythicLevel > 0) then
-	if (C_ChallengeMode.IsChallengeModeActive() == true) then
+	if (mythicLevel and mythicLevel > 0) then
 		if (mythicDungeonCharts.ChartTable and mythicDungeonCharts.ChartTable.Running and bossInfo) then
 
 			local tCopiedBossInfo = Details:GetFramework().table.copy({}, bossInfo)
