e903e121b6be0f61f7d04f014fd3254ebd8434c2
Tercio Jose <terciob19@hotmail.com>
2025-06-16 19:12:52 -0300

    Small fixes, framework update, performance improvements, community implementations.

--- boot.lua
+++ boot.lua
@@ -17,8 +17,8 @@
 		end
 		local addonName, Details222 = ...
 		local version, build, date, tvs = GetBuildInfo()
-		Details.build_counter = 13601
-		Details.alpha_build_counter = 13601 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 13616
+		Details.alpha_build_counter = 13616 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
