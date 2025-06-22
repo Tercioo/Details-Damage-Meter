1effca974f58ba37be928ae36b0e897ec0895545
Tercio Jose <terciob19@hotmail.com>
2025-06-22 14:06:12 -0300

    Fix damage done to skull marked targets

--- core/parser.lua
+++ core/parser.lua
@@ -1623,6 +1623,8 @@
 		end
 
 		--actor owner (if any)
+		targetRaidFlags = bitBand(targetRaidFlags, COMBATLOG_OBJECT_RAIDTARGET_MASK)
+
 		if (ownerActor) then --se for dano de um Pet
 			ownerActor.total = ownerActor.total + amount --e adiciona o dano ao pet
 
