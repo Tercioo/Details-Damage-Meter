919778550184224d34ad06bba91d92929943e154
Tercio Jose <terciob19@hotmail.com>
2025-08-06 15:26:46 -0300

    Open Raid version up

--- Libs/LibOpenRaid/LibOpenRaid.lua
+++ Libs/LibOpenRaid/LibOpenRaid.lua
@@ -59,7 +59,7 @@ end
 
 local major = "LibOpenRaid-1.0"
 
-local CONST_LIB_VERSION = 163
+local CONST_LIB_VERSION = 164
 
 if (LIB_OPEN_RAID_MAX_VERSION) then
     if (CONST_LIB_VERSION <= LIB_OPEN_RAID_MAX_VERSION) then
