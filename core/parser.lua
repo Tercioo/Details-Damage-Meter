edb8615da32455e791bace88924489c85190e16f
Lynn <kjarli@gmail.com>
2026-01-16 19:30:33 +0100

    Use a function that's more likely to be the correct one

--- core/parser.lua
+++ core/parser.lua
@@ -50,7 +50,7 @@
 	local _GetSpellInfo = Details.getspellinfo
     local GetSpellInfo = Details222.GetSpellInfo
 	local isERA = detailsFramework.IsClassicWow()
-    local isCLASSIC = detailsFramework.IsCataWow() or detailsFramework.IsPandaWow() or isERA or detailsFramework.IsWotLKWow() or detailsFramework.IsBCC()
+    local isCLASSIC = detailsFramework.IsCataWow() or detailsFramework.IsPandaWow() or isERA or detailsFramework.IsWotLKWow() or detailsFramework.IsTBCWow()
 	local _tempo = time()
 	_ = nil
 
@@ -7893,4 +7893,5 @@ end
 --]=]
 
 --end
+
 
