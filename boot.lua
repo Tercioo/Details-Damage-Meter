0b0dad5135de8f83e8c17bd96334d4899cd389d1
Flamanis <Flamanis@users.noreply.github.com>
2026-03-01 12:08:47 -0600

    Proper version up

--- boot.lua
+++ boot.lua
@@ -17,8 +17,8 @@
 		end
 		local addonName, Details222 = ...
 		local version, build, date, tvs = GetBuildInfo()
-		Details.build_counter = 14697
-		Details.alpha_build_counter = 14697 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 14700
+		Details.alpha_build_counter = 14700 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
