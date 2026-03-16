cd9c26dd1a7281986ad62e5b50f98faf6528b877
Flamanis <Flamanis@users.noreply.github.com>
2026-03-16 02:53:26 -0500

    Use GetOldSchoolTalentInformation if on tbc when resetting the spec cache

--- core/gears.lua
+++ core/gears.lua
@@ -145,15 +145,19 @@ function Details:ResetSpecCache(forced)
 		Details:Destroy(Details.cached_specs)
 
 		if (Details.track_specs) then
-			local playerSpec = DetailsFramework.GetSpecialization()
-			if (type(playerSpec) == "number") then
-				local specId = DetailsFramework.GetSpecializationInfo(playerSpec)
-				if (type(specId) == "number") then
-					local playerGuid = UnitGUID(Details.playername)
-					if (playerGuid) then
-						Details.cached_specs[playerGuid] = specId
-					end
-				end
+            if (DetailsFramework.IsTBCWow()) then
+                Details.GetOldSchoolTalentInformation()
+            else
+                local playerSpec = DetailsFramework.GetSpecialization()
+                if (type(playerSpec) == "number") then
+                    local specId = DetailsFramework.GetSpecializationInfo(playerSpec)
+                    if (type(specId) == "number") then
+                        local playerGuid = UnitGUID(Details.playername)
+                        if (playerGuid) then
+                            Details.cached_specs[playerGuid] = specId
+                        end
+                    end
+                end
 			end
 		end
 
