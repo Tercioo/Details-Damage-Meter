93a3363a0e5ed7e77ca507b49577b4621aa53608
Tercio Jose <terciob19@hotmail.com>
2026-04-07 13:42:00 -0300

    Release, see description for list of changes
    - Added Potion usage for Midnight.
    - Added reports for Midnight.
    - Added Asian abbreviations.
    - Added options to control the player name length.
    - Improved the combat time.
    - Improved realm name removal.
    - Fix 'overall' word not always showing in the window title.

--- boot.lua
+++ boot.lua
@@ -19,6 +19,8 @@
 		local version, build, date, tvs = GetBuildInfo()
 		Details.build_counter = 14850
 		Details.alpha_build_counter = 14850 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 14895
+		Details.alpha_build_counter = 14895 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
