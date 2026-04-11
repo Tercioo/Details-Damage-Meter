5ee27fb52df89ef29b3d824f9fa91e11131e1db1
Tercio Jose <terciob19@hotmail.com>
2026-04-11 14:15:29 -0300

    Version Up - Change log in the description
    - Fix error when hovering over a spec icon while in a boss fight.
    - Fix Potion Usage display showing secret errors.
    - Fix Left Text padding which wasn't working in retail.
    - Patch 12.0.5 preparations.
    - Add Wago API.

--- boot.lua
+++ boot.lua
@@ -17,8 +17,8 @@
 		end
 		local addonName, Details222 = ...
 		local version, build, date, tvs = GetBuildInfo()
-		Details.build_counter = 14902
-		Details.alpha_build_counter = 14902 --if this is higher than the regular counter, use it instead
+		Details.build_counter = 14930
+		Details.alpha_build_counter = 14930 --if this is higher than the regular counter, use it instead
 		Details.dont_open_news = true
 		Details.game_version = version
 		Details.userversion = version .. " " .. Details.build_counter
